3c883bc5c96bb686b3a24d088828effdd57d3713
SinitsinDima <Tyuiu.SinitsinDV@outlook.com>
2024-10-16 17:58:50 +0500

    Task6

---
+++ Tyuiu.SinitsinDV.Sprint2.Task6.V2.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.SinitsinDV.Sprint2.Task6.V2.Lib;
+namespace Tyuiu.SinitsinDV.Sprint2.Task6.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int value = 3;
+            var wait = "Весна";
+            var res = ds.FindMonthSeason(value);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SinitsinDV.Sprint2.Task6.V2/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.SinitsinDV.Sprint2.Task6.V2.Lib;
+namespace Tyuiu.SinitsinDV.Sprint2.Task6.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Синицин Д.В. | ПКТб-24-1";
+
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("* Спринт #2                                         *");
+            Console.WriteLine("* Тема: Получение результата из switch              *");
+            Console.WriteLine("* Задание #6                                        *");
+            Console.WriteLine("* Вариант #2                                        *");
+            Console.WriteLine("* Выполнил: Синицин Дмитрий Владимирович | ПКТб-24-1*");
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                          *");
+            Console.WriteLine("* Написать программу, которая использует сокращенную*");
+            Console.WriteLine("* форму записи оператора switch вычисляет требуемое *");
+            Console.WriteLine("* значение и возвращает результат.                  *");
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                  *");
+            Console.WriteLine("*****************************************************");
+
+            Console.WriteLine("Введите номер месяца: ");
+            var value = Convert.ToInt32(Console.ReadLine());
+            var res = ds.FindMonthSeason(value);
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                        *");
+            Console.WriteLine("*****************************************************");
+            Console.WriteLine("Время года " + res);
+
+        }
+    }
+}
